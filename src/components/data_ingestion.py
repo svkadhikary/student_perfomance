61d809801c0b5cafce234c65967e9ad004e893f1
svkadhikary <svkadhikary7@gmail.com>
2023-03-14 18:35:39 +0530

    model training

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -8,6 +8,7 @@ from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 
 from src.components.data_transformation import DataTransformation
+from src.components.model_trainer import ModelTrainer
 
 
 @dataclass
@@ -55,5 +56,11 @@ if __name__=="__main__":
     train_data,test_data=obj.initiate_data_ingestion("notebook/data/stud.csv")
 
     data_transformation=DataTransformation()
-    data_transformation.initiate_data_transformation(train_data,test_data)
+    train_arr, test_arr, _ = data_transformation.initiate_data_transformation(train_data,test_data)
+
+    model_trainer = ModelTrainer()
+
+    best_score = model_trainer.initiate_model_trainer(train_arr, test_arr)
+
+    print("Best score: ", best_score)
 
--- src/utils.py
+++ src/utils.py
@@ -2,8 +2,10 @@ import os
 import sys
 import dill
 
-from src.exception import CustomException
+from sklearn.model_selection import GridSearchCV
+from sklearn.metrics import r2_score
 
+from src.exception import CustomException
 
 def save_object(obj, file_path):
     try:
@@ -15,3 +17,35 @@ def save_object(obj, file_path):
 
     except Exception as e:
         raise CustomException(e, sys)
+
+
+def evaluate_models(X_train, y_train, X_test, y_test, models:dict, hyperparams:dict):
+    try:
+        report = {}
+        for model_name, model in models.items():
+            if model_name in hyperparams:
+
+                param_grid = hyperparams[model_name]
+                grid_search = GridSearchCV(model, param_grid, scoring='r2', n_jobs=3, cv = 5 , verbose=5)
+                grid_search.fit(X_train, y_train)
+
+                best_model = grid_search.best_estimator_
+
+                y_train_pred = best_model.predict(X_train)
+                y_test_pred = best_model.predict(X_test)
+
+                train_score = r2_score(y_train, y_train_pred)
+                test_score = r2_score(y_test, y_test_pred)
+
+                report[model_name] = test_score
+            
+            else:
+                model.fit(X_train, y_train)
+                report[model_name] = r2_score(y_test, model.predict(X_test))
+
+        return report
+
+
+    except Exception as e:
+        raise CustomException(e, sys)
+    
